fba134f257bc1f8cc69d5414b8484194150b6397
Pranav Pokale <pranavpokale50034@gmail.com>
2023-06-22 14:44:25 +0530

    submitting code-Thu Jun 22 2023 14:44:24 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [sum, setSum] = useState(0)
+  const [number, setNumber] = useState(0)
+
+  useEffect(()=>{
+    setSum(Number(sum)+Number(number))
+  },[number])
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+        <input onChange={e=>setNumber(e.target.value)} type="number" />
+        <p>Sum: {sum}</p>
     </div>
   )
 }
